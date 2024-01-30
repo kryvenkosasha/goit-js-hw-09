586cae7f747ae17bf51d2c8ecb6b6b2f08cc9388
kryvenkosasha <143883449+kryvenkosasha@users.noreply.github.com>
2024-01-30 22:15:05 +0100

    Update 2-form.js

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -13,8 +13,8 @@ document.addEventListener('DOMContentLoaded', function () {
 form.addEventListener('submit', onFormSubmit);
 
 function onFormInput() {
-  const email = form.elements.email.value;
-  const message = form.elements.message.value;
+  const email = form.elements.email.value.trim(); 
+  const message = form.elements.message.value.trim();
 
   const data = {
     email,
@@ -28,6 +28,12 @@ function onFormSubmit(event) {
   event.preventDefault();
 
   const formData = loadFromLocalStorage(STORAGE_KEY);
+
+  if (!formData || !formData.email || !formData.message) {
+    console.error('Не всі поля заповнені');
+    return;
+  }
+
   console.log(formData);
 
   form.reset();
